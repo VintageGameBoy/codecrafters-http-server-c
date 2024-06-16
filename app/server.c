f8e56edf7622183307ac75ced73bf12b38faec6f
JerryHuang <JerryHuang@Outlook.Jp>
2024-06-17 03:00:32 +0800

    codecrafters submit [skip ci]

--- app/server.c
+++ app/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
@@ -8,7 +9,15 @@
 #include <unistd.h>
 
 const char *response_Ok = "HTTP/1.1 200 OK\r\n\r\n";
-const char *response_NotFound = "HTTP/1.1 404 OK\r\n\r\n";
+const char *response_NotFound = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+bool isRequestTooLarge(const char *request, int maxSize) {
+    int requestSize = strlen(request);
+    if (requestSize > maxSize) {
+        return true;
+    }
+    return false;
+}
 
 int main() {
     setbuf(stdout, NULL);
@@ -51,10 +60,40 @@ int main() {
     printf("Waiting for a client to connect...\n");
     client_addr_len = sizeof(client_addr);
 
-    int fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
     printf("Client connected\n");
-    int sendResult = send(fd, response_Ok, strlen(response_Ok), 0);
-    close(fd);
+
+    const int MAX_REQUEST_SIZE = 10 * 1024 * 1024; // 10MB
+
+    // 接收HTTP请求
+    int bufferSize = 4096;
+    char *httpRequest = (char *) malloc(bufferSize);
+    int bytesRead = recv(client_fd, httpRequest, bufferSize, 0);
+    if (bytesRead != 0) {//说明还有没读完的 先不做操作先
+
+    }
+
+    char method[10];
+    char path[100];
+
+    // 解析请求方法
+    sscanf(httpRequest, "%s", method);
+    // 解析请求路径
+    sscanf(strchr(httpRequest, ' ') + 1, "%s", path);
+
+    if (strcmp(method, "GET") == 0 || strcmp(method, "POST") == 0) {
+
+        if (strcmp(path, "/") == 0 || strcmp(path, "/index.html") == 0) {
+            int sendResult = send(client_fd, response_Ok, strlen(response_Ok), 0);
+        } else {
+            int sendResult = send(client_fd, response_NotFound, strlen(response_NotFound), 0);
+        }
+
+
+    }
+
+
+    close(client_fd);
     close(server_fd);
 
     return 0;
